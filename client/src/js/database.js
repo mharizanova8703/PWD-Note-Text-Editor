bb03e7043bfebddcc288678dd232da268ff76fc5
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2022-01-14 12:56:23 -0500

    creating a register route
    using stalewhilerevalidate

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,13 +1,13 @@
 import { openDB } from 'idb'
 
 const initdb = async () =>
-  openDB('Note-Editor', 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('Note-Editor')) {
+      if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists')
         return
       }
-      db.createObjectStore('Note-Editor', {
+      db.createObjectStore('jate', {
         keyPath: 'id',
         autoIncrement: true,
       })
@@ -20,13 +20,13 @@ export const putDb = async (edit) => {
   console.log('Post to the database')
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('Note-Editor', 1)
+  const contactDb = await openDB('jate', 1)
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('Note-Editor', 'readwrite')
+  const tx = contactDb.transaction('jate', 'readwrite')
 
   // Open up the desired object store.
-  const store = tx.objectStore('Note-Editor')
+  const store = tx.objectStore('jate')
 
   // Use the .add() method on the store and pass in the content.
   const request = store.add({
@@ -43,13 +43,13 @@ export const getDb = async () => {
   console.log('GET from the database')
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('Note-Editor', 1)
+  const contactDb = await openDB('jate', 1)
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('Note-Editor', 'readwrite')
+  const tx = contactDb.transaction('jate', 'readwrite')
 
   // Open up the desired object store.
-  const store = tx.objectStore('Note-Editor')
+  const store = tx.objectStore('jate')
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll()
